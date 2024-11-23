1ded7cc8ea7757c7cf01bdbaaca76e39befe3cdd
mopolita <albanrichard48@gmail.com>
2024-11-23 20:18:36 +0100

    added some stuff to the form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,390 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button2 = new Button();
+            comboBox1 = new ComboBox();
+            textBox1 = new TextBox();
+            button3 = new Button();
+            textBox7 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            comboBox2 = new ComboBox();
+            label3 = new Label();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            comboBox3 = new ComboBox();
+            label4 = new Label();
+            textBox6 = new TextBox();
+            textBox8 = new TextBox();
+            comboBox4 = new ComboBox();
+            label5 = new Label();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            comboBox5 = new ComboBox();
+            label6 = new Label();
+            textBox11 = new TextBox();
+            textBox12 = new TextBox();
+            comboBox6 = new ComboBox();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(126, 56);
+            button1.TabIndex = 0;
+            button1.Text = "Charger un fichier";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(653, 15);
+            button2.Name = "button2";
+            button2.Size = new Size(135, 53);
+            button2.TabIndex = 1;
+            button2.Text = "Sauvegarder un modèle";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(188, 112);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(50, 28);
+            comboBox1.TabIndex = 2;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(136, 112);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(46, 27);
+            textBox1.TabIndex = 7;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(298, 444);
+            button3.Name = "button3";
+            button3.Size = new Size(180, 29);
+            button3.TabIndex = 18;
+            button3.Text = "Générer les statisiques";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(265, 112);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(31, 27);
+            textBox7.TabIndex = 19;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(244, 116);
+            label1.Name = "label1";
+            label1.Size = new Size(19, 20);
+            label1.TabIndex = 25;
+            label1.Text = "+";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(244, 232);
+            label2.Name = "label2";
+            label2.Size = new Size(19, 20);
+            label2.TabIndex = 29;
+            label2.Text = "+";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(265, 228);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(31, 27);
+            textBox2.TabIndex = 28;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(136, 228);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(46, 27);
+            textBox3.TabIndex = 27;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(188, 228);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(50, 28);
+            comboBox2.TabIndex = 26;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(244, 337);
+            label3.Name = "label3";
+            label3.Size = new Size(19, 20);
+            label3.TabIndex = 33;
+            label3.Text = "+";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(265, 333);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(31, 27);
+            textBox4.TabIndex = 32;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(136, 333);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(46, 27);
+            textBox5.TabIndex = 31;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(188, 333);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(50, 28);
+            comboBox3.TabIndex = 30;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(595, 113);
+            label4.Name = "label4";
+            label4.Size = new Size(19, 20);
+            label4.TabIndex = 37;
+            label4.Text = "+";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(616, 109);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(31, 27);
+            textBox6.TabIndex = 36;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(487, 109);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(46, 27);
+            textBox8.TabIndex = 35;
+            // 
+            // comboBox4
+            // 
+            comboBox4.FormattingEnabled = true;
+            comboBox4.Location = new Point(539, 109);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(50, 28);
+            comboBox4.TabIndex = 34;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(595, 233);
+            label5.Name = "label5";
+            label5.Size = new Size(19, 20);
+            label5.TabIndex = 41;
+            label5.Text = "+";
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(616, 229);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(31, 27);
+            textBox9.TabIndex = 40;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(487, 229);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(46, 27);
+            textBox10.TabIndex = 39;
+            // 
+            // comboBox5
+            // 
+            comboBox5.FormattingEnabled = true;
+            comboBox5.Location = new Point(539, 229);
+            comboBox5.Name = "comboBox5";
+            comboBox5.Size = new Size(50, 28);
+            comboBox5.TabIndex = 38;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(595, 337);
+            label6.Name = "label6";
+            label6.Size = new Size(19, 20);
+            label6.TabIndex = 45;
+            label6.Text = "+";
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(616, 333);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(31, 27);
+            textBox11.TabIndex = 44;
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(487, 333);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(46, 27);
+            textBox12.TabIndex = 43;
+            // 
+            // comboBox6
+            // 
+            comboBox6.FormattingEnabled = true;
+            comboBox6.Location = new Point(539, 333);
+            comboBox6.Name = "comboBox6";
+            comboBox6.Size = new Size(50, 28);
+            comboBox6.TabIndex = 42;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(136, 87);
+            label7.Name = "label7";
+            label7.Size = new Size(45, 20);
+            label7.TabIndex = 46;
+            label7.Text = "Force";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(487, 87);
+            label8.Name = "label8";
+            label8.Size = new Size(70, 20);
+            label8.TabIndex = 47;
+            label8.Text = "Dextérité";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(136, 205);
+            label9.Name = "label9";
+            label9.Size = new Size(89, 20);
+            label9.TabIndex = 48;
+            label9.Text = "Constitution";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(487, 205);
+            label10.Name = "label10";
+            label10.Size = new Size(58, 20);
+            label10.TabIndex = 49;
+            label10.Text = "Pouvoir";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(136, 310);
+            label11.Name = "label11";
+            label11.Size = new Size(81, 20);
+            label11.TabIndex = 50;
+            label11.Text = "Apparence";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(487, 310);
+            label12.Name = "label12";
+            label12.Size = new Size(75, 20);
+            label12.TabIndex = 51;
+            label12.Text = "Education";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 485);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(textBox11);
+            Controls.Add(textBox12);
+            Controls.Add(comboBox6);
+            Controls.Add(label5);
+            Controls.Add(textBox9);
+            Controls.Add(textBox10);
+            Controls.Add(comboBox5);
+            Controls.Add(label4);
+            Controls.Add(textBox6);
+            Controls.Add(textBox8);
+            Controls.Add(comboBox4);
+            Controls.Add(label3);
+            Controls.Add(textBox4);
+            Controls.Add(textBox5);
+            Controls.Add(comboBox3);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(textBox3);
+            Controls.Add(comboBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox7);
+            Controls.Add(button3);
+            Controls.Add(textBox1);
+            Controls.Add(comboBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Lovecraftian Creature Roller";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private ComboBox comboBox1;
+        private TextBox textBox1;
+        private Button button3;
+        private TextBox textBox7;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private ComboBox comboBox2;
+        private Label label3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private ComboBox comboBox3;
+        private Label label4;
+        private TextBox textBox6;
+        private TextBox textBox8;
+        private ComboBox comboBox4;
+        private Label label5;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private ComboBox comboBox5;
+        private Label label6;
+        private TextBox textBox11;
+        private TextBox textBox12;
+        private ComboBox comboBox6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
     }
 }
