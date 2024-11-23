b92bf030e29446d420a75e7b236a2f1c12da0f5d
mopolita <albanrichard48@gmail.com>
2024-11-24 00:34:37 +0100

    Update Form1.cs
    changed to respect rules

--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,17 @@ namespace CreatureRoller
             InitializeComponent();
         }
 
-        private void boutonCharger_Click(object sender, EventArgs e)
+        private void BoutonCharger_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void boutonGenerer_Click(object sender, EventArgs e)
+        private void BoutonGenerer_Click(object sender, EventArgs e)
         {
-
+            // Récupérer les valeurs des TextBox et de la ComboBox
+            string NbFor = textNbFor.Text; // Contenu de la 1ère TextBox
+            string ModFor = textModFor.Text; // Contenu de la 2ème TextBox
+            string comboForVal = comboFor.SelectedItem!.ToString(); // Valeur sélectionnée dans la ComboBox
         }
     }
 }
