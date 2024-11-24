cbe90599100e0a691069a3aaa61343721c15078f
mopolita <albanrichard48@gmail.com>
2024-11-24 10:51:39 +0100

    continued bougenerer method

--- Form1.cs
+++ Form1.cs
@@ -15,9 +15,12 @@ namespace CreatureRoller
         private void BoutonGenerer_Click(object sender, EventArgs e)
         {
             // Récupérer les valeurs des TextBox et de la ComboBox
-            string NbFor = textNbFor.Text; // Contenu de la 1ère TextBox
-            string ModFor = textModFor.Text; // Contenu de la 2ème TextBox
-            string comboForVal = comboFor.SelectedItem!.ToString(); // Valeur sélectionnée dans la ComboBox
+            Stat force = new Stat(textNbFor.Text, textModFor.Text, comboFor.SelectedItem!.ToString());
+            Stat dex = new Stat(textNbDex.Text, textModDex.Text, comboDex.SelectedItem!.ToString());
+            Stat con = new Stat(textNbCon.Text, textModCon.Text, comboCon.SelectedItem!.ToString());
+            Stat pou = new Stat(textNbPou.Text, textModPou.Text, comboPou.SelectedItem!.ToString());
+            Stat app = new Stat(textNbApp.Text, textModApp.Text, comboApp.SelectedItem!.ToString());
+            Stat edu = new Stat(textNbEdu.Text, textModEdu.Text, comboEdu.SelectedItem!.ToString());
         }
     }
 }
