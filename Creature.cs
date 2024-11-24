9cb71d69311ef33c070c185babde0cf32658ad6d
mopolita <albanrichard48@gmail.com>
2024-11-24 20:52:44 +0100

    Create Creature.cs
    created the class created

---
+++ Creature.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CreatureRoller
+{
+	/// <summary>
+	/// Summary description for Class1
+	/// </summary>
+	public class Creature
+	{
+		public string Name { get; }
+		public Stat For { get; }
+		public Stat Dex { get; }
+		public Stat Con { get; }
+		public Stat Pou { get; }
+		public Stat App { get; }
+		public Stat Edu { get; }
+
+		public Creature()
+		{
+			Name = "";
+			For = new Stat();
+			Dex = new Stat();
+			Con = new Stat();
+			Pou = new Stat();
+			App = new Stat();
+			Edu = new Stat();
+		}
+	}
+}
