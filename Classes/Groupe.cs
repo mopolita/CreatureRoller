bdc4bb42205e127343da4b68892a63cb7ca6d986
mopolita <albanrichard48@gmail.com>
2024-12-14 18:33:18 +0100

    Update Groupe.cs

--- Classes/Groupe.cs
+++ Classes/Groupe.cs
@@ -1,25 +1,21 @@
-﻿using System;
-
-namespace CreatureRoller 
+﻿namespace CreatureRoller 
 {
 	public class Groupe
 	{
-		public GroupBox StatBox;
+		private readonly List<string> comboRange = new List<string> { "d4", "d6", "d8", "d10", "d12", "d20", "d100" };
+
+        public GroupBox StatBox;
 		private NumericUpDown NumericUpDownNb;
 		private ComboBox ComboStat;
 		private NumericUpDown NumericUpDownMod;
 		private readonly Label Resultat;
 		private TextBox TextResultat;
 
-		private Stat Stat;
-
-		public Groupe(Stat stat, List<string> comboRange, Point location)
+		public Groupe(Stat stat, Point location)
 		{
-			Stat = stat;
-
 			StatBox = new GroupBox
 			{
-				Text = Stat.Name,
+				Text = stat.Name,
 				Location = location,
 				Size = new Size(195, 60)
 			};
@@ -76,11 +72,45 @@ namespace CreatureRoller
 			StatBox.Controls.Add(Resultat);
 		}
 
-		public void UpdateStat(Stat stat) 
-		{
-			Stat = stat;
-			NumericUpDownNb.Value = new decimal(new int[] { 1, 0, 0, 0 });
+		private int FacesToIndex(int faces)
+        {
+			int index = 0;
+            switch (faces)
+            {
+                case 4:
+					index = 0;
+					break;
+                case 6:
+                    index = 1;
+                    break;
+                case 8:
+                    index = 2;
+                    break;
+                case 10:
+                    index = 3;
+                    break;
+				case 12:
+                    index = 4;
+                    break;
+                case 20:
+                    index = 5;
+                    break;
+				case 100:
+                    index = 6;
+                    break;
+                default:
+                    index = 0;
+                    break;
+            }
+            return index;
+		}
 
+		public void UpdateGroupe(Stat stat) 
+		{
+			NumericUpDownNb.Value = new decimal(stat.NumDice);
+			NumericUpDownMod.Value = new decimal(stat.Modifier);
+			StatBox.Text = stat.Name;
+            ComboStat.SelectedIndex = FacesToIndex(stat.Faces);
         }
 	}
 }
